5c4c8b7f12fab6d22f14f5f6f2a0536ff97bb43d
yaotsung <er0106er@gmail.com>
2020-04-28 02:07:20 +0800

    Import buefy and router and can work now
    add prettierrc
    test frame in home page
    finish search way in about page

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import router from "./router";
+import axios from "axios";
+import VueAxios from "vue-axios";
 
+Vue.use(VueAxios, axios);
 Vue.config.productionTip = false
-
+Vue.use(Buefy)
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="home">
+    <div class="columns">
+      <div class="column is-four-fifths">is-four-fifths</div>
+      <div class="column">Auto</div>
+      <div class="column">Auto</div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-three-quarters">is-three-quarters</div>
+      <div class="column">Auto</div>
+      <div class="column">Auto</div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-two-thirds">is-two-thirds</div>
+      <div class="column">Auto</div>
+      <div class="column">Auto</div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-three-fifths">is-three-fifths</div>
+      <div class="column">Auto</div>
+      <div class="column">Auto</div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-half">is-half</div>
+      <div class="column">Auto</div>
+      <div class="column">Auto</div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-two-fifths">is-two-fifths</div>
+      <div class="column">Auto</div>
+      <div class="column">Auto</div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-one-third">is-one-third</div>
+      <div class="column">Auto</div>
+      <div class="column">Auto</div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-one-quarter">is-one-quarter</div>
+      <div class="column">Auto</div>
+      <div class="column">Auto</div>
+    </div>
+
+    <div class="columns">
+      <div class="column is-one-fifth">is-one-fifth</div>
+      <div class="column">Auto</div>
+      <div class="column">Auto</div>
+    </div>
+  </div>
+</template>
